3475f093c9c0aaa4a439a784506b9ba14f46d7dc
Dennis Nyström <dennis.nystrom@gmail.com>
2018-01-14 16:52:05 +0100

    FindOnlyOwned mixin

---
+++ system/common/mixins/find-only-owned.js
@@ -0,0 +1,18 @@
+
+module.exports = (Model, options) => {
+    'use strict';
+
+    Model.beforeRemote('find', (ctxWithArgs, data, next) => {
+        const ctx = ctxWithArgs.args || {};
+        ctx.options = ctx.options || {};
+
+        ctx.filter = ctx.filter || {};
+        ctx.filter.where = ctx.filter.where || {};
+
+        const token = ctx.options.accessToken || {};
+        ctx.filter.where['hubUserId'] = token.userId;
+        
+        next();
+    });
+    
+}
